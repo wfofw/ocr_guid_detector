3d7ccbf7c630c8177c230e9a2e0861c73467c354
wfofw <uunreall0@gmail.com>
2026-08-28 05:36:05 +0300

    build(deps): add fastapi and uvicorn dependencies

--- demo.py
+++
@@ -1,8 +0,0 @@
-from guid_detector.extractor import find_guids_by_stripes
-from PIL import Image
-
-image = Image.open("sample_screenshot_2.jpeg")
-
-# Localization and extraction
-guids = find_guids_by_stripes(image)
-print("Found GUIDs:", guids)
